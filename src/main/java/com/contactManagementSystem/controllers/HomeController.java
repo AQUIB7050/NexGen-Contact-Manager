b7c1170cfbfbba4040b3307abcf72eb1fa984fa1
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-07-30 20:16:42 +0530

    initial commit

---
+++ src/main/java/com/contactManagementSystem/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.contactManagementSystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model m) {
+		m.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model m) {
+		m.addAttribute("title", "About - Smart Contact Manager");
+		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signUp(Model m) {
+		m.addAttribute("title", "Register - Smart Contact Manager");
+		return "signup";
+	}
+
+}
---
+++ target/classes/static/js/script.js
@@ -0,0 +1,5 @@
+/**
+ * 
+ */
+
+console.log("Hi, I Am Attached");
