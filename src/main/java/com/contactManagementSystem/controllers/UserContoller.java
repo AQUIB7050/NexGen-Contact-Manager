1fad79a79ddca604fccfe0f171cf66f3ccd70cce
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-08-05 21:56:06 +0530

    contact form added

--- src/main/java/com/contactManagementSystem/controllers/UserContoller.java
+++ src/main/java/com/contactManagementSystem/controllers/UserContoller.java
@@ -6,30 +6,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.contactManagementSystem.dao.UserRepository;
+import com.contactManagementSystem.entities.Contact;
 import com.contactManagementSystem.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserContoller {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 	
 	
-	@GetMapping("/index")
-	public String dashboard(Model model,Principal principal) {
+	//It will run for all the methods.
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
+
 		String username = principal.getName();
-		
+
 		User user = this.userRepository.getUserByUserName(username);
-		
-		model.addAttribute("user",user);
-		
-		
-		return "normal/user_dashboard";
+
+		model.addAttribute("user", user);
+
 	}
 	
+
+	@GetMapping("/index")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("title", "user - dashboard");
+
+		return "normal/user_dashboard";
+	}
+
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		model.addAttribute("title", "add contact");
+		model.addAttribute("contact", new Contact());
+		
+		return "normal/add_contact";
+	}
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,3 +3,19 @@
  */
 
 console.log("Hi, I Am Attached");
+
+const toggleSidebar = () => {
+
+    if($(".sidebar").is(":visible")){
+
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left","0%");
+
+    } else {
+
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left","20%");
+
+    }
+};
+
